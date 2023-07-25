7ba3c3237dd2f19fac2ab5c71e3bda5d6a18885f
bugradostt <bugradostt@gmail.com>
2023-07-25 15:47:04 +0300

    first commit

---
+++ Casgem_Mediator/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using Casgem_Mediator.MeditorPattern.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Casgem_Mediator.Controllers
+{
+    public class DefaultController : Controller
+    {
+        readonly IMediator _mediator;
+
+        public DefaultController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _mediator.Send(new GetProductQuery());
+            return View(values);
+        }
+    }
+}
---
+++ Casgem_Mediator/Migrations/20230725124540_mig_edit.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Casgem_Mediator.Migrations
+{
+    public partial class mig_edit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductBran",
+                table: "Products",
+                newName: "ProductBrand");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductBrand",
+                table: "Products",
+                newName: "ProductBran");
+        }
+    }
+}
